c58880ff67ec86819afb460b5bff0d5e6b54dd70
khaleelyb <ikhalil520@gmail.com>
2026-04-14 15:41:58 +0100

    Update copyright year in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 dark:bg-gray-900 text-white">
       <div className="container mx-auto px-4 py-6 text-center">
-        <p>&copy; 2024 Kano Marketplace. All rights reserved.</p>
+        <p>&copy; 2026 Kano Marketplace. All rights reserved.</p>
       </div>
     </footer>
   );
